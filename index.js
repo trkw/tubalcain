d6851ff49a4e6b2d0a90d2337a73e2ceba6c9cd8
trkw <trkw@trkw.me>
2016-09-06 01:03:10 +0900

    let -> const heroku

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import express from 'express';
-let app = express();
+import express from "express"
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/dist'))
+const app = express()
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.set("port", (process.env.PORT || 5000))
+app.use(express.static(__dirname + "/dist"))
+
+app.get("/", function(request, response) {
+  response.send("Hello World!")
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen(app.get("port"), function() {
+  console.log("Node app is running at localhost:" + app.get("port"))
 })
