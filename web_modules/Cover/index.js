362bb5c0a9fa9dc365c2ee757b9aa16ab8472c0c
trkw <trkw@trkw.me>
2016-09-06 23:53:01 +0900

    Add p5.js
    Add p5.js
    
    p5 resolve min.js
    
    I don't think much of that…
    
    componentDidMount in require
    
    Add isBrowser and resizeCanvas
    
    Drop resolve p5.js

---
+++ web_modules/Cover/index.js
@@ -0,0 +1,29 @@
+import React from "react"
+const isBrowser = typeof window !== "undefined"
+const p5 = isBrowser ? require("p5/lib/p5.min") : undefined
+
+const sketch = function(p) {
+  p.setup = function() {
+    p.createCanvas(p.windowWidth, p.windowHeight)
+  }
+
+  p.draw = function() {
+    p.background(55, 110)
+    p.noStroke()
+    p.fill(255)
+    p.ellipse(p.mouseX, p.mouseY, 80, 80)
+  }
+  p.windowResized = function() {
+    p.resizeCanvas(p.windowWidth, p.windowHeight)
+  }
+}
+
+export default class Cover extends React.Component {
+  componentDidMount() {
+    new p5(sketch)
+  }
+
+  render() {
+    return <div></div>
+  }
+}
