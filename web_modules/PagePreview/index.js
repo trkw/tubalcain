c8390a8e2bb69002183a5e51d632ad04aae467ce
Masaya Kazama <miyan@t-p.jp>
2016-09-23 21:13:17 +0900

    WIP

--- web_modules/PagePreview/index.js
+++ web_modules/PagePreview/index.js
@@ -1,22 +1,33 @@
 import React, { PropTypes } from "react"
 import { Link } from "react-router"
+import styles from "./index.css"
+import moment from "moment"
 
-const PagePreview = ({ __url, title, date }) => {
-  const pageDate = date ? new Date(date) : null
+const PagePreview = ({ __url, title, date, authors, description }) => {
+  const pageDate = moment(date ? new Date(date) : null)
 
   return (
     <div>
       <Link to={ __url }>
         { title }
       </Link>
+      <small>
+        { description }
+      </small>  
+      {
+        authors && authors[0] &&
+        <div className={ styles.author }>
+          { authors[0] }
+        </div>
+      }
       {
         pageDate &&
-        <small>
+        <div className={ styles.date }>
           { " " }
           <time key={ pageDate.toISOString() }>
-            { pageDate.toDateString() }
+            { pageDate.locale("ja").format("YYYY/MM/DD dddd") }
           </time>
-        </small>
+        </div>
       }
     </div>
   )
@@ -26,6 +37,8 @@ PagePreview.propTypes = {
   __url: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   date: PropTypes.string,
+  authors: PropTypes.array.isRequired,
+  description: PropTypes.string.isRequired,
 }
 
 export default PagePreview
--- web_modules/PagesList/index.js
+++ web_modules/PagesList/index.js
@@ -1,17 +1,20 @@
 import React, { PropTypes } from "react"
-
 import PagePreview from "../PagePreview"
+import styles from "./index.css"
 
 const PagesList = ({ pages }) => {
+  console.log(pages)
   return (
     <div>
     {
       pages.length
       ? (
-        <ul>
+        <ul className={ styles.list }>
         {
           pages.map((page) => (
-            <li key={ page.title }><PagePreview { ...page } /></li>
+            <li className={ styles.item }
+              key={ page.title }
+            ><PagePreview { ...page } /></li>
           ))
         }
         </ul>
